edf91c47806caee4e5bbcbff281a7dfa534220fc
unknown <candeviolam@gmail.com>
2023-07-11 11:18:39 -0300

    paginación (botones prev y next en la pág de los personajes), renderizado de detalle del personaje desde las tarjetas de personajes (no puedo llegar hasta ahí pq no me anda el botón el Detalle desde CharactersPage.jsx)

--- src/pages/characters-detail/CharactersDetailPage.jsx
+++ src/pages/characters-detail/CharactersDetailPage.jsx
@@ -1,21 +1,55 @@
-import { useEffect } from "react";
+import CustomLoader from "../../components/loader/CustomLoader";
+import { GetCharacterById } from "../../services/RickAndMortyService";
+//                  importamos ahora si el useState que nos estaba faltando
+import { useEffect, useState } from "react";
 //       hook de react-router-dom -> p/recibir los parámetros de la ruta(?
-import { useParams } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
+
+import "./CharacterDetail.css";
 
 export const CharacterDetailPage = () => {
   //    params -> corresponde en éste caso a "id" de abajo en el path, por eso puedo hacerle destructurtig y obtenerlo directam como id
   //             lo llamo p/recibir los parámetros de la ruta(?
   //    me devuelve (el useParams(?) un obj con una propiedad que se llama "id", que es el valor de lo que le voy a estar enviando luego de la barra diagonal /
   const { id } = useParams(); //useParams me devuelve el obj que enviamos por params
+  //traemos los estados de CharactersPage(?
+  //                                         useState inicializado como obj vacío
+  const [character, setCharacter] = useState({});
+  const [loader, setLoader] = useState(false);
 
   //p/hacer una petición al servidor -> usamos el id p/hacer la petición a la api
   useEffect(() => {
+    setLoader(true); // p/que se cargue como hacía con los personajes en CharactersPage
     //el fetch que teníamos acá, movido a services, solo dejo un .then y el .catch (? -> lo extraje a un servicio externo
     //importamos la func desde el servicio, con el id(?
     GetCharacterById({ id })
-      .then((result) => console.log(result))
-      .catch((err) => console.log(err));
-  }); //sin dependencia, necesitamos que se ejecuto una sola vez
+      //una vez que se terminó y volvió el resultado , setLoader en false
+      .then((result) => {
+        setCharacter(result); //estamos almacenando el valor del personaje una vez que se terminó de cargar y volvemos a pasar el loader a false
+        setLoader(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoader(false);
+      });
+  }, []); //acá adentro estamos llamando al setLoader ahora, y debería ponerle la dependencia del loader (de arriba del useState) acá adentro p/q funcione correctamente -> pero le paso el array vacío pq no quiero que se cargue de nuevo el componente
 
-  return <h1>Hola Detalle Personajes</h1>;
+  //ya me trae el detalle del personaje, como ya hice el character arriba (useState) puedo llamarle la imagen
+  return (
+    //vamos a hacer de nuevo el renderizado condicional
+    //vamos a preguntar si éso está cargando, y si está cargando ponemos el CustomLoader
+    // una vez que terminó de cargar, asumo que volvió la info del servidor y puedo mostrarla (con el name y la image en este caso)
+    //agrego {} envolviendo todo dentro del div p/poder agregar lógica adentro
+    <div className="character-container">
+      {loader ? (
+        <CustomLoader loading={loader} />
+      ) : (
+        <section>
+          <img src={character.image} />
+          <h3>{character.name}</h3>
+          <NavLink to="/characters" className="btn btn-dark">Volver</NavLink>
+        </section>
+      )}
+    </div>
+  );
 };
--- src/pages/characters/CharactersPage.jsx
+++ src/pages/characters/CharactersPage.jsx
@@ -9,6 +9,7 @@ export const CharactersPage = () => {
   //     utilizar este characters en el return p/renderizar un componente u otro
   const [characters, setCharacters] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1); // abajo en el div, una vez que se hayan renderizado mis personajes, vamos a devolver otro div más..
 
   useEffect(() => {
     //antes de empezar a cargar los personajes, antes de llamar a la base, vamos a colocar la carga (vamos a empezar a mostrarle al usuario que estamos haciendo una carga)
@@ -16,7 +17,8 @@ export const CharactersPage = () => {
     //tenemos ya la petición y estamos usando una func p/reutilizar el código
     // acá hice un fetch que me lo lleve al archivo de RickAndMortyService.jsx adentro de la carpeta services
     //usando la función asíncrona creada desde un servicio externo que tratamos acá como una promesa (con el .then y el .catch)
-    GetCharacters({ page: 1 })
+    //              acá simplemente paso page (sin el :1 que tenía )
+    GetCharacters({ page })
       //       desestructurar el res que tenía aca y pasarle directam el results que me viene en consola (hacer console.log de results) de la api con destructuring -> desestructurando el obj y poder recibir directam el array (de personajes de la api, en consola)
       .then(({ results }) => {
         //hacemos el setTimeOut y le pasamos los 2segs al final, para forzar que aparezca ese tiempito el spinner
@@ -33,7 +35,8 @@ export const CharactersPage = () => {
         console.log(err);
         setLoading(false);
       });
-  }, []);
+  }, [page]); //paso las dependencias necesarias, pq quiero que cuando page cambie (botones de prev y next) se ejecute de nuevo el método
+  //  cuando cambie page, el efecto se vuelve a ejecutar entero
 
   return (
     //renderizado condicional
@@ -45,14 +48,25 @@ export const CharactersPage = () => {
     //       characters del useState de arriba, adentro recibo el personaje (de las tarjetas(?) y el index
     <div className="centered">
       {!loading ? (
-        <div className="container-fluid contenedor-tarjetas">
-          {characters.map((character, index) => {
-            return <CharacterCard character={character} key={index} />;
-          })}
+        <div className="container-fluid contenedor-tarjs-but">
+          <div className="contenedor-tarjetas">
+            {characters.map((character, index) => {
+              return <CharacterCard character={character} key={index} />;
+            })}
+          </div>
+          <div className="controls">
+            <button onClick={() => setPage(page - 1)} className="btn btn-dark">
+              Previous
+            </button>
+            <button onClick={() => setPage(page + 1)} className="btn btn-dark">
+              Next
+            </button>
+          </div>
         </div>
       ) : (
         <CustomLoader loading={loading} />
       )}
     </div>
+    //   agregamos otro div dentro de el div de contendor de tarjetas, con botones, para hacer el paginado con el useState de arriba
   );
 };
